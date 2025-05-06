f848d0fdd97015c8e0e65b8d7ecbdce2d46dd7d4
rishav76dev <rjrishav037@gmail.com>
2025-05-06 20:35:22 +0530

    fn for intialize poll added

--- programs/voting/src/lib.rs
+++ programs/voting/src/lib.rs
@@ -4,10 +4,25 @@ declare_id!("DXBjq1Fydn1DU1GE4biWnM2UWa1NDSKGmBZ4co8rEZpi");
 
 #[program]
 pub mod voting {
+    use std::fmt::DebugStruct;
+
     use super::*;
     //step 1
-    pub fn initialize(_ctx: Context<InitializePoll>, _poll_id: u64 ) -> Result<()> {
-        
+    pub fn initialize_poll(_ctx: Context<InitializePoll>, 
+        //Step 5
+        poll_name: String,
+        description: String,
+        poll_start: u64,
+        poll_end: u64,
+    ) -> Result<()> {
+        //Step 6 then we move to testing this code
+        let poll = &mut _ctx.accounts.poll_account;
+        poll.poll_name = poll_name;
+        poll.poll_description = description;
+        poll.poll_voting_start = poll_start;
+        poll.poll_voting_end = poll_end;
+        poll.candidate_amout = 0;
+
         Ok(())
     }
 }
@@ -44,5 +59,5 @@ pub struct PollAccount{
     pub poll_description: String,
     pub poll_voting_start: u64,
     pub poll_voting_end: u64,
-    pub poll_option_index: u64,
+    pub candidate_amout: u64,
 }
